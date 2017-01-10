6b31e532b17bbe34161554a246919f25cd1f0505
Hikita Kamyshenko <nikita.kamyshenko@mail.ru>
2017-01-10 23:13:13 +0300

    Add example.

--- Concurrency/src/main/java/my/test/MainConcurrency.java
+++ Concurrency/src/main/java/my/test/MainConcurrency.java
@@ -1,7 +1,65 @@
 package my.test;
 
 class MainConcurrency {
-    public static void main(String[] args){
-
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("=========");
+            go();
+        }
     }
+
+    private static void go() throws InterruptedException {
+        Balance balance = new Balance();
+
+        for (int i = 0; i < 100; i++) {
+            goThread2(balance);
+            goThread1(balance);
+        }
+
+        Thread.sleep(10 * 100);
+
+
+
+
+        System.out.println("Balance: " + balance.getBalance());
+    }
+
+    private static void goThread2(final Balance balance) {
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //System.out.println("runnable1");
+                balance.inc();
+            }
+        });
+        thread1.start();
+    }
+
+    private static void goThread1(final Balance balance) {
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //System.out.println("runnable2");
+                balance.dec();
+            }
+        });
+        thread2.start();
+    }
+
+    static class Balance {
+        private int balance;
+
+        public void inc() {
+            balance++;
+        }
+
+        public void dec() {
+            balance--;
+        }
+
+        public int getBalance() {
+            return balance;
+        }
+    }
+
 }
